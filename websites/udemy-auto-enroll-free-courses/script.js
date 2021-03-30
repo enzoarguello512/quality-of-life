21b2bfca378c53288a34b3e9877e5da86a68ab69
Enzo Arguello <xlsones003@gmail.com>
2021-03-30 20:05:16 -0300

    updated udemy-auto-enroll script (minor bugs fixed)

--- websites/udemy-auto-enroll-free-courses/script.js
+++ websites/udemy-auto-enroll-free-courses/script.js
@@ -1,4 +1,16 @@
 window.onload = () => {
+
+  //main variables
+
+  //This is the button on the main page before proceeding to finalize the purchase
+  const mainSelector = 'button[data-purpose="buy-this-course-button"]';
+
+  const checkFrequencyInMs = 1000;
+  const timeoutInMs = 7000;
+
+  //It is the time it will take before clicking on asd, this is set in case your browser / internet connection is slow, the lower it is, the more likely it will fail (a loop could be used but this may also slow down the process)
+  const timeToBeInteractive = 1500;
+
   function closeWindow() {
     // close the browser window if you are already enrolled in the course (it doesn't work 100% of the time)
     window.open('', '_parent', '');
@@ -6,15 +18,14 @@ window.onload = () => {
   }
 
   function waitForElementToDisplay(selector, checkFrequencyInMs, timeoutInMs) {
-    console.log('llamada');
     var startTimeInMs = Date.now();
     (function loopSearch() {
-      let buyBtn = document.querySelectorAll(selector)[0];
+      const buyBtn = document.querySelectorAll(selector)[0];
       if (buyBtn != null && buyBtn != undefined) {
         let buyBtnInnerText = buyBtn.innerText;
         if (buyBtnInnerText == "Enroll now") {
           // click enroll now button
-          document.querySelectorAll(selector)[0].click();
+          setTimeout(document.querySelectorAll(selector)[0].click(), timeToBeInteractive);
         }
 
         if (buyBtnInnerText == "Go to course") {
@@ -29,21 +40,24 @@ window.onload = () => {
             return false;
           }
 
-          try {
-            // select the country code/value
-            document.getElementById('billingAddressCountry').value = 'AR';
-            // click on the enroll button
-            document.querySelectorAll('button[type="submit"]')[2].click();
-            closeWindow;
-          }
-          catch {
-            console.log('reloco');
-            loopSearch();
-          }
+          //-------Disabled because you can't select a specific option (maybe in some future update udemy will return it to how it was before)
+
+          //try {
+          // select the country code/value
+          //document.getElementById('billingAddressCountry').value = 'AR';
+          // click on the enroll button
+          //setTimeout(document.querySelectorAll('button[type="submit"]')[2].click(), timeToBeInteractive);
+          //closeWindow;
+          //}
+          //catch {
+          //loopSearch();
+          //}
+
+          //-----------------------------------------------
         }, checkFrequencyInMs);
       }
     })();
   }
 
-  waitForElementToDisplay('button[data-purpose="buy-this-course-button"]', 1000, 7000);
+  waitForElementToDisplay(mainSelector, checkFrequencyInMs, timeoutInMs);
 }
